53f02c98b8b54855a9d46eb411c4c49464036860
Manuel Cid <manuel.cid@rai.usc.es>
2021-11-19 14:06:02 +0100

    prueba

--- src/Cliente/Loader.java
+++ src/Cliente/Loader.java
@@ -8,32 +8,46 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
  *
  * @author itocd
  */
-public class VIinicio extends Application {
+public class Loader extends Application {
+    Font font = Font.loadFont("file:./Recuersos/Typo_Round_Bold_Demo.otf", 45);
+    Font font2 = Font.loadFont("file:./Recuersos/Typo_Round_Regular_Demo.otf", 18);
     
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(VIinicio.class.getResource("Inicio.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Loader.class.getResource("Inicio.fxml"));
+        //Hay que crear un escenario y dentro poner escenas, es decir, la ventana y después los componentes
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setScene(scene);
         primaryStage.show();
-       
+    }
+    
+    
+    @FXML
+    private Label labelHost;
+    
+    public void prueba(){
+        labelHost.setText("Prueba");
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch();
+        launch();        
     }
     
 }
