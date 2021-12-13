1435f3568876636225043f86ab4eccaebe682b8d
Manuel Cid <manuel.cid@rai.usc.es>
2021-12-13 13:22:40 +0100

    josefina

--- src/Cliente/UI/PrincipalController.java
+++ src/Cliente/UI/PrincipalController.java
@@ -8,6 +8,7 @@ import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -43,6 +44,7 @@ public class PrincipalController {
                 actualizarPanel();
             });
         });
+        
     }
 
     synchronized void registrarMensaje(String emisor, String mensaje) {
